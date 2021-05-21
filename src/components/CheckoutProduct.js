c9a031448322cfbeb83e0ca2da43ff81971193ef
natasha8 <nat.parisella@gmail.com>
2021-05-21 20:59:08 +0200

    try to fix problem

--- src/components/CheckoutProduct.js
+++ src/components/CheckoutProduct.js
@@ -13,10 +13,11 @@ const CheckoutProduct = ({
 	rating,
 }) => {
 	return (
-		<div className="grid grid-cols-5 ">
+		<div className="grid grid-cols-5 bg-black">
+			<h1 color="red">bla bla bla</h1>
 			<Image src={image} height={200} width={200} objectFit="contain" />
 			<div className="col-span-3 mx-5">
-				<p>{title}</p>
+				<p>{title} </p>
 				{Array(rating)
 					.fill()
 					.map((_, i) => (
--- src/pages/checkout.js
+++ src/pages/checkout.js
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import Image from "next/image";
 import { useSelector } from "react-redux";
 import CheckoutProduct from "../components/CheckoutProduct";
@@ -6,43 +7,49 @@ import { selectItems } from "../slices/basketSlice";
 
 const Checkout = () => {
 	const items = useSelector(selectItems);
+	console.log(items);
 	return (
 		<div className="bg-gray-100">
+			<Head>
+				<title>Amazon Checkout</title>
+			</Head>
 			<Header />
-			<main className="lg:flex max-w-screen-2xl ms-auto">
+			<main className="lg:flex max-w-screen-2xl mx-auto">
 				{/*left*/}
-				<div className="flex-grow m5 shadow-sm">
+				<div className="flex-grow m-5 shadow-sm">
 					<Image
 						src="https://links.papareact.com/ikj"
-						height={250}
 						width={1020}
+						height={250}
 						objectFit="contain"
 					/>
 				</div>
-				{/*right*/}
 
 				<div className="flex flex-col p-5 space-y-10 bg-white">
-					<h1 className="text-3xl border-b pb-4">
+					<h1 className="text-2xl border-b pb-4">
 						{items.length === 0
 							? "Your Amazon Basket is empty"
 							: "Shopping Basket"}
-						<h1>
-							{items.map((item, i) => {
-								<CheckoutProduct
-									key={i}
-									id={item.id}
-									title={item.title}
-									price={item.price}
-									description={item.description}
-									category={item.category}
-									image={item.image}
-									hasPrime={item.hasPrime}
-									rating={item.rating}
-								/>;
-							})}
-						</h1>
 					</h1>
+					{items.map((item, i) => {
+						{
+							console.log("bucchin", item);
+						}
+						<CheckoutProduct
+							key={i}
+							id={item.id}
+							title={item.title}
+							price={item.price}
+							description={item.description}
+							category={item.category}
+							image={item.image}
+							hasPrime={item.hasPrime}
+							rating={item.rating}
+						/>;
+					})}
 				</div>
+				{/*right*/}
+				<div></div>
 			</main>
 		</div>
 	);
